e0a3a0ff24e28626567e2a1c3fdfd6c676177536
larcyK <40230524+larcyK@users.noreply.github.com>
2024-07-16 00:44:17 +0900

    add clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { createSignal } from "solid-js";
 import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open } from '@tauri-apps/api/dialog'
+import { writeText, readText } from '@tauri-apps/api/clipboard';
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = createSignal("");
   const [name, setName] = createSignal("");
+  const [clipboard, setClipboard] = createSignal("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name: name() }));
+  }
+
+  async function setClipboardText() {
+    await writeText(clipboard());
+  }
+
+  function selectFile() {
+    console.log('select file');
+    open({ directory: false, multiple: false }).then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.error(error);
+    });
   }
 
   return (
@@ -44,6 +60,23 @@ function App() {
         />
         <button type="submit">Greet</button>
       </form>
+
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setClipboardText();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setClipboard(e.currentTarget.value)}
+          placeholder="Enter text to set clipboard..."
+        />
+        <button type="submit">Set Clipboard</button>
+      </form>
+
+      <button onClick={selectFile}>Click to open dialog</button>
 
       <p>{greetMsg()}</p>
     </div>
