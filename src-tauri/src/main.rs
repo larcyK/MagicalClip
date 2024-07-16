3776019737dfeed527ae3f1f5b749c6514bc43a8
larcyK <40230524+larcyK@users.noreply.github.com>
2024-07-16 11:48:57 +0900

    add bridge to clipboard

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,8 +4,21 @@
 use tauri::Manager;
 use tokio::{
     io::{self, AsyncWriteExt, BufReader},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream}, sync::Mutex,
 };
+use arboard::Clipboard;
+use std::sync::{mpsc, Arc};
+use lazy_static::lazy_static;
+
+struct AppState {
+    sendDataQueue: Vec<Vec<u8>>,
+}
+
+lazy_static! {
+    static ref APP_STATE: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState {
+        sendDataQueue: Vec::new(),
+    }));
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -20,6 +33,26 @@ fn front_to_back(event: tauri::Event) {
     );
 }
 
+async fn monitor_clipboard() {
+    let mut clipboard = Clipboard::new().unwrap();
+    let mut last_clipboard = clipboard.get_text().unwrap();
+    loop {
+        let current_clipboard = clipboard.get_text().unwrap();
+        if current_clipboard != last_clipboard {
+            println!("Clipboard changed: {}", current_clipboard);
+            APP_STATE.lock().await.sendDataQueue.push(current_clipboard.as_bytes().to_vec());
+            last_clipboard = current_clipboard;
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+}
+
+fn update_clipboard(data: Vec<u8>) {
+    let mut clipboard = Clipboard::new().unwrap();
+    let text = std::str::from_utf8(&data).unwrap();
+    clipboard.set_text(text).unwrap();
+}
+
 #[tauri::command]
 async fn start_listening() -> Result<(), String> {
     println!("Starting server...");
@@ -27,7 +60,7 @@ async fn start_listening() -> Result<(), String> {
         Ok(listener) => listener,
         Err(err) => return Err(err.to_string()),
     };
-
+    
     loop {
         match listener.accept().await {
 
@@ -35,24 +68,33 @@ async fn start_listening() -> Result<(), String> {
                 println!("Connected to server at {}", addr);
 
                 loop {
-                    stream.writable().await.map_err(|e| e.to_string())?;
-                    if let Err(e) = stream.write_all(b"hello world\n").await {
-                        println!("Failed to write to socket; err = {:?}", e);
-                        break;
-                    }
-
-                    let buf = &mut [0; 4096];
+                    let buf = &mut [0; 1 << 16];
                     match stream.try_read(buf) {
                         Ok(n) => {
                             if n == 0 {
                                 println!("Connection closed by server");
                                 break;
                             }
+                            let data = buf[..n].to_vec();
                             println!("Received {} bytes", n);
-                            println!("{:?}", std::str::from_utf8(&buf[..n]));
+                            println!("Data: {:?}", std::str::from_utf8(&data));
+                            update_clipboard(data);
                         }
                         Err(e) => {
                             println!("Failed to read from socket; err = {:?}", e);
+                        }
+                    }
+
+                    let mut state = APP_STATE.lock().await;
+                    while !state.sendDataQueue.is_empty() {
+                        let data = state.sendDataQueue.remove(0);
+                        match stream.write_all(&data).await {
+                            Ok(_) => {
+                                println!("Sent {} bytes", data.len());
+                            }
+                            Err(e) => {
+                                println!("Failed to write to socket; err = {:?}", e);
+                            }
                         }
                     }
 
@@ -79,6 +121,13 @@ fn main() {
                     .emit_all("back-to-front", "ping frontend".to_string())
                     .unwrap();
                 std::thread::sleep(std::time::Duration::from_secs(1))
+            });
+            std::thread::spawn(|| {
+                tokio::runtime::Builder::new_current_thread()
+                    .enable_all()
+                    .build()
+                    .unwrap()
+                    .block_on(monitor_clipboard());
             });
             let id = app.listen_global("front-to-back", front_to_back);
             Ok(())
